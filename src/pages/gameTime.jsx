daa9e64a4aa5f41f73575abb1c8e8df3c4a5c2fc
YoselinMejia <20011412@galileo.edu>
2024-10-25 02:43:06 -0600

    arreglos varios x2000

--- src/pages/gameTime.jsx
+++ src/pages/gameTime.jsx
@@ -1,28 +1,32 @@
 import React, { useEffect } from 'react';
 import * as echarts from 'echarts';
+import './style.css'
 
 const GameTime = () => {
-  // Opciones de la gráfica semanal
+  //Gráfica semanal
   const weeklyOption = {
     title: {
       text: 'Tiempo de Juego Semanal',
       left: 'center',
       textStyle: {
         color: '#4715e2',
-        fontSize: 18,
+        fontSize: 20,
         fontWeight: 'bold',
+        fontFamily: 'Poppins, sans-serif',
       },
     },
     xAxis: {
       type: 'category',
       data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-      name: 'Días de la Semana',
+    
       nameTextStyle: {
-        color: '#4715e2',
+        color: '#5b5e76',
         fontSize: 14,
+        fontFamily: 'Poppins, sans-serif',
       },
       axisLabel: {
-        color: '#333',
+        color: '#5b5e76',
+        fontFamily: 'Poppins, sans-serif',
       },
     },
     yAxis: {
@@ -31,84 +35,95 @@ const GameTime = () => {
       nameTextStyle: {
         color: '#4715e2',
         fontSize: 14,
+        fontFamily: 'Poppins, sans-serif',
       },
       axisLabel: {
-        color: '#333',
+        color: '#5b5e76',
+        fontFamily: 'Poppins, sans-serif',
       },
     },
     series: [
       {
-        data: [120, 200, 150, 80, 70, 110, 130],
+        data: [1, 2, 5, 5, 2, 1, 3],
         type: 'bar',
         itemStyle: {
           color: '#ffc243',
+          barBorderRadius: [15, 15, 0, 0],
         },
+        barCategoryGap: '10%',
       },
     ],
   };
 
-  // Opciones de la gráfica mensual
-  const monthlyOption = {
+  //Gráfica Nivel de convivencia
+  const coexistenceOption = {
     title: {
-      text: 'Tiempo de Juego Mensual',
+      text: 'Nivel de Convivencia',
       left: 'center',
       textStyle: {
         color: '#4715e2',
-        fontSize: 18,
+        fontSize: 20,
         fontWeight: 'bold',
+        fontFamily: 'Poppins, sans-serif',
       },
     },
     xAxis: {
       type: 'category',
-      data: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
-      name: 'Semanas del Mes',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    
       nameTextStyle: {
-        color: '#4715e2',
+        color: '#5b5e76',
         fontSize: 14,
+        fontFamily: 'Poppins, sans-serif',
       },
       axisLabel: {
-        color: '#333',
+        color: '#5b5e76',
+        fontFamily: 'Poppins, sans-serif',
       },
     },
     yAxis: {
       type: 'value',
-      name: 'Horas Jugadas',
+      name: 'Convivencia',
       nameTextStyle: {
         color: '#4715e2',
         fontSize: 14,
+        fontFamily: 'Poppins, sans-serif',
       },
       axisLabel: {
-        color: '#333',
+        color: '#5b5e76',
+        fontFamily: 'Poppins, sans-serif',
       },
     },
     series: [
       {
-        data: [300, 450, 500, 400],
+        data: [1, 2, 2, 5, 2, 1, 5],
         type: 'bar',
         itemStyle: {
           color: '#4715e2',
+          barBorderRadius: [15, 15, 0, 0],
         },
+        barCategoryGap: '10%',
       },
     ],
   };
 
   useEffect(() => {
-    // Inicializar la gráfica semanal
+    //Gráfica semanal
     const weeklyChartDom = document.getElementById('weekly-chart');
     const weeklyChart = echarts.init(weeklyChartDom);
     weeklyChart.setOption(weeklyOption);
 
-    // Inicializar la gráfica mensual
-    const monthlyChartDom = document.getElementById('monthly-chart');
-    const monthlyChart = echarts.init(monthlyChartDom);
-    monthlyChart.setOption(monthlyOption);
+    //Gráfica Convivencia
+    const coexistenceChartDom = document.getElementById('coexistence-chart');
+    const coexistenceChart = echarts.init(coexistenceChartDom);
+    coexistenceChart.setOption(coexistenceOption);
   }, []);
 
   return (
     <div>
       <h1 style={{ fontWeight: 'bold', textAlign: 'center' }}>Tiempo de Juego</h1>
       <div id="weekly-chart" style={{ width: '100%', height: 400 }}></div>
-      <div id="monthly-chart" style={{ width: '100%', height: 400, marginTop: '20px' }}></div>
+      <div id="coexistence-chart" style={{ width: '100%', height: 400, marginTop: '20px' }}></div>
     </div>
   );
 };
